8da1bd6a09bd48129304c5cd3bfe499c875fc1a9
juan18506 <juan185067@gmail.com>
2023-10-12 18:10:40 -0300

    updated toggleTodo

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -58,7 +58,13 @@ const addTodo = (description) => {
  * @param {String} todoId 
  */
 const toggleTodo = (todoId) => {
-  throw new Error('Not implemented');
+  state.todos = state.todos.map(todo => {
+    if (todo.id === todoId) {
+      todo.done = !todo.done;
+    }
+
+    return todo;
+  });
 }
 
 /**
