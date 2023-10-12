8d1f414590150e9d7f0b2232a64c913891cb4523
juan18506 <juan185067@gmail.com>
2023-10-12 18:36:21 -0300

    updated render

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -7,10 +7,23 @@ import { Todo } from '../models/todo.model';
 export const createTodoHTML = (todo) => {
   if (!todo) throw new Error('todo is required');
 
-  const html = `<h1>${todo.description}</h1>`;
+  const {id, description, done} = todo;
+
+  const html = `
+    <div class="view">
+        <input class="toggle" type="checkbox" ${done ? 'checked' : ''}>
+        <label>${description}</label>
+        <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a todo MVC template">
+  `;
 
   const liElement = document.createElement('li');
   liElement.innerHTML = html;
+  liElement.setAttribute('data-id', id);
+  if (done) {
+    liElement.classList.add('completed');
+  }
 
   return liElement;
 }
