cd6514f875b21144f28a4d53ff565abf2df79755
juan18506 <juan185067@gmail.com>
2023-10-12 18:47:19 -0300

    added listener to add todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -4,7 +4,7 @@ import { renderTodos } from './use-cases';
 
 const ElementIDs = {
   TodoList: '.todo-list',
-
+  NewTodoInput: '#new-todo-input',
 }
 
 /**
@@ -25,4 +25,16 @@ export const App = (elementId) => {
     displayTodos();
   })()
 
+  // Referencias HTML
+  const newDescriptionINput = document.querySelector(ElementIDs.NewTodoInput);
+
+  // Listeners
+  newDescriptionINput.addEventListener('keyup', (event) => {
+    if (event.keyCode !== 13) return;
+    if (event.target.value.trim().length === 0) return;
+
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = '';
+  });
 }
