da5d7a47ad2b5a00b90ce59297ff82e6d7b69328
juan18506 <juan185067@gmail.com>
2023-10-12 17:55:34 -0300

    created store functions

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -19,6 +19,53 @@ const initStore = () => {
   console.log(state);
 }
 
+const loadStore = () => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = (todoId) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = (todoId) => {
+  throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+  throw new Error('Not implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+  throw new Error('Not implemented');
+}
+
 export default {
   initStore,
+  loadStore,
+  addTodo,
+  toggleTodo,
+  deleteTodo,
+  deleteCompleted,
+  setFilter,
+  getCurrentFilter,  
 }
